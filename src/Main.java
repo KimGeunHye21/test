57f58eb0b08c729d4aad41234f4b6f24848986df
GeunHye <wheeinjj417@gmail.com>
2023-09-22 21:40:57 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i<n; i++) list.add(input.nextInt());
+
+        Collections.sort(list);
+
+        StringBuilder sb = new StringBuilder();
+        for (int value : list) sb.append(value).append('\n');
+
+        System.out.println(sb);
+
+
+    }
+}
